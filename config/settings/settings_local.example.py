fd200bb3084395fc545f2fc2e2df201c9d3cc404
anastasia <anastasia.aizman@gmail.com>
2023-07-28 14:15:12 -0400

    add - better settings system to be able to keep track of keys

---
+++ config/settings/settings_local.example.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+# DEDUPLICATIONS_DIR = "/real-dedup-dir"
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "evictions_local",
+#         "USER": "",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
+#         "PORT": "",
+#     }
+# }
