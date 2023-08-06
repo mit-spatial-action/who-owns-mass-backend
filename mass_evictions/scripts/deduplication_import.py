201403d61355539ef6510e43e856cf8aa0414aee
anastasia <anastasia.aizman@gmail.com>
2023-08-05 21:31:15 -0400

    fix - fix fix

--- mass_evictions/scripts/deduplication_import.py
+++ mass_evictions/scripts/deduplication_import.py
@@ -2,28 +2,27 @@ import os
 from tqdm import tqdm
 import geopandas as gpd
 import pandas as pd
+from django.db import IntegrityError
+
 from config.settings import DEDUPLICATIONS_DIR
 from mass_evictions.models import Parcel, OwnerGroup, Owner, Corp
 
-row_num_default = 100000
+default_chunksize = 10**6
 
 
-def import_parcels(filename, row_num=row_num_default):
+def import_parcels(filename):
     parcels_file = os.path.join(DEDUPLICATIONS_DIR, filename)
-    rows = 0
-    while True:
-        rows += row_num
-        gdf = gpd.read_file(parcels_file, rows=rows)
-        parcels = gdf.to_dict("records")
-        [
-            Parcel.objects.create(
-                object_id=parcel_dict["OBJECTID"],
-                loc_id=parcel_dict["LOC_ID"],
-                town_id=parcel_dict["TOWN_ID"],
-                geometry=str(parcel_dict["geometry"]),
-            )
-            for parcel_dict in tqdm(parcels)
-        ]
+    gdf = gpd.read_file(parcels_file)
+    parcels = gdf.to_dict("records")
+    [
+        Parcel.objects.create(
+            object_id=parcel_dict["OBJECTID"],
+            loc_id=parcel_dict["LOC_ID"],
+            town_id=parcel_dict["TOWN_ID"],
+            geometry=str(parcel_dict["geometry"]),
+        )
+        for parcel_dict in tqdm(parcels)
+    ]
 
 
 def create_owner_objects(owner):
@@ -48,54 +47,56 @@ def create_owner_objects(owner):
     )
 
 
-def import_owners(filename, row_num=row_num_default):
+def import_owners(filename, chunksize=default_chunksize):
     owners_file = os.path.join(DEDUPLICATIONS_DIR, filename)
-    rows = 0
-    while True:
-        print("Getting the next %s to %s rows" % (str(rows), str(rows + row_num)))
-        rows += row_num
-        df = pd.read_csv(
-            owners_file,
-            nrows=row_num,
-            sep="|",
-            converters={
-                "prop_id": str,
-                "loc_id": str,
-                "fy": int,
-                "use_code": str,
-                "city": str,
-                "owner1": str,
-                "own_addr": str,
-                "own_state": str,
-                "own_zip": str,
-                "co": str,
-                "zip": str,
-                "name_address": str,
-                "group": str,
-                "id_corp": str,
-                "count": int,
-            },
-        )
-        owners = df.to_dict("records")
-        [create_owner_objects(owner) for owner in tqdm(owners)]
+    with pd.read_csv(
+        owners_file,
+        sep="|",
+        converters={
+            "prop_id": str,
+            "loc_id": str,
+            "fy": int,
+            "use_code": str,
+            "city": str,
+            "owner1": str,
+            "own_addr": str,
+            "own_state": str,
+            "own_zip": str,
+            "co": str,
+            "zip": str,
+            "name_address": str,
+            "group": str,
+            "id_corp": str,
+            "count": int,
+        },
+        chunksize=chunksize,
+    ) as reader:
+        for df in reader:
+            owners = df.to_dict("records")
+            [create_owner_objects(owner) for owner in tqdm(owners)]
 
 
 def create_corp_objects(corp):
     group, _ = OwnerGroup.objects.get_or_create(id=corp["group_network"])
-    Corp.objects.create(id=corp["id"], name=corp["entityname"], group=group)
+    try:
+        Corp.objects.create(id=corp["id"], name=corp["entityname"], group=group)
+    except IntegrityError:
+        existing_corp = Corp.objects.get(id=corp["id"])
+        if (
+            existing_corp.name != corp["entityname"]
+            or existing_corp.group.id != group.id
+        ):
+            breakpoint()
+        else:
+            pass
 
 
-def import_corps(filename, row_num=row_num_default):
+def import_corps(filename):
     corps_file = os.path.join(DEDUPLICATIONS_DIR, filename)
-    rows = 0
-    while True:
-        print("Getting the next %s to %s rows" % (str(rows), str(rows + row_num)))
-        rows += row_num
-        df = pd.read_csv(
-            corps_file,
-            nrows=row_num,
-            sep="|",
-            converters={"id": str, "name": str, "group_network": str},
-        )
-        corps = df.to_dict("records")
-        [create_corp_objects(corp) for corp in tqdm(corps)]
+    df = pd.read_csv(
+        corps_file,
+        sep="|",
+        converters={"id": str, "name": str, "group_network": str},
+    )
+    corps = df.to_dict("records")
+    [create_corp_objects(corp) for corp in tqdm(corps)]
