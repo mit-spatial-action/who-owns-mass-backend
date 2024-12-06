571711a2e12344ab321f5c628b4d5569ac4decca
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-12-06 09:21:20 -0500

    set up tokenauthentication

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ router.register(r'sites', SiteViewset, basename='site')
 router.register(r'metacorps', MetaCorpViewset, basename='metacorp')
 
 urlpatterns = [
+    path('api/auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
