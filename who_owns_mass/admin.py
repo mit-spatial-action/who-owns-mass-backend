e2e2bb046143044351f249fb67bf22fe9a905701
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-01-16 12:38:03 -0500

    #91 basic admin pages

--- who_owns_mass/admin.py
+++ who_owns_mass/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import (
+    MetaCorp,
+    Municipality,
+    ZipCode,
+    BlockGroup,
+    Tract,
+    ParcelPoint,
+    Address,
+    Site,
+    Company,
+    Owner,
+    SiteToOwner,
+    Role,
+    Officer,
+    OfficerToRole,
+)
 
-# Register your models here.
+admin.site.register(MetaCorp)
+admin.site.register(Municipality)
+admin.site.register(ZipCode)
+admin.site.register(BlockGroup)
+admin.site.register(Tract)
+admin.site.register(ParcelPoint)
+admin.site.register(Address)
+admin.site.register(Site)
+admin.site.register(Company)
+admin.site.register(Owner)
+admin.site.register(SiteToOwner)
+admin.site.register(Role)
+admin.site.register(Officer)
+admin.site.register(OfficerToRole)
