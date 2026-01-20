c4e11650ae34c86496fb1228c6d9756a82657407
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-01-20 10:49:53 -0500

    modify `static/` location

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ import environ
 env = environ.Env()
 env.read_env()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
